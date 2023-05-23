f47aa48abaab205e5121e5157d647f64d25e85c3
Lucianor01 <lucianorosario02@gmail.com>
2023-05-23 17:12:14 +0200

    api json dischi

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-
+            apiUrl: 'apis/listaDischi.php',
+            data: '',
         }
     },
     methods: {
-
+        chiamataApi() {
+            axios.get(this.apiUrl)
+                .then((res) => {
+                    this.data = res.data;
+                    console.log(res.data);
+                })
+        }
     },
     mounted() {
+        this.chiamataApi();
     }
 }).mount('#app')
